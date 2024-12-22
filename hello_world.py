799ec801e1cbb2c785d69fb78c4228a89b04ab28
Fuzhuo Huang <huang.ford@gmail.com>
2024-12-22 09:51:01 -0800

    First hello world file

---
+++ hello_world.py
@@ -0,0 +1,4 @@
+while True:
+    name = input("Enter Your Name: ")
+    print(name + " is having so much fun at the water park!She is eating ice cream and playing with her friends!She loves playing , she is having a great time!Her friends and her are making a video!") 
+
