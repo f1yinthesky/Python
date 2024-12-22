89fc93c0aaaace39fcb16505bae48b5d376f7ec6
Fuzhuo Huang <huang.ford@gmail.com>
2024-12-22 10:58:45 -0800

    Improve the print_board function 1. Shows who is the player letter 2. Add map from number to board

--- examples/tictactoe_example.py
+++ examples/tictactoe_example.py
@@ -2,15 +2,15 @@
 
 import random
 
-def drawBoard(board):
+def drawBoard(board, letter):
     # This function prints out the board that it was passed.
-
+    print("\nYou are " + letter + ".")
     # "board" is a list of 10 strings representing the board (ignore index 0)
-    print(board[7] + '|' + board[8] + '|' + board[9])
-    print('-+-+-')
-    print(board[4] + '|' + board[5] + '|' + board[6])
-    print('-+-+-')
-    print(board[1] + '|' + board[2] + '|' + board[3])
+    print(board[7] + '|' + board[8] + '|' + board[9] + '   7|8|9')
+    print('-+-+-   -+-+-')
+    print(board[4] + '|' + board[5] + '|' + board[6] + '   4|5|6')
+    print('-+-+-   -+-+')
+    print(board[1] + '|' + board[2] + '|' + board[3] + '   1|2|3')
 
 def inputPlayerLetter():
     # Lets the player type which letter they want to be.
@@ -137,17 +137,17 @@ while True:
     while gameIsPlaying:
         if turn == 'player':
             # Player's turn.
-            drawBoard(theBoard)
+            drawBoard(theBoard, playerLetter)
             move = getPlayerMove(theBoard)
             makeMove(theBoard, playerLetter, move)
 
             if isWinner(theBoard, playerLetter):
-                drawBoard(theBoard)
+                drawBoard(theBoard, playerLetter)
                 print('Hooray! You have won the game!')
                 gameIsPlaying = False
             else:
                 if isBoardFull(theBoard):
-                    drawBoard(theBoard)
+                    drawBoard(theBoard, playerLetter)
                     print('The game is a tie!')
                     break
                 else:
@@ -159,12 +159,12 @@ while True:
             makeMove(theBoard, computerLetter, move)
 
             if isWinner(theBoard, computerLetter):
-                drawBoard(theBoard)
+                drawBoard(theBoard, playerLetter)
                 print('The computer has beaten you! You lose.')
                 gameIsPlaying = False
             else:
                 if isBoardFull(theBoard):
-                    drawBoard(theBoard)
+                    drawBoard(theBoard, playerLetter)
                     print('The game is a tie!')
                     break
                 else:
