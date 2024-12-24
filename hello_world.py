36df7b44dc9fc9a307d8d5a6605555600347efc5
Fuzhuo Huang <huang.ford@gmail.com>
2024-12-23 18:02:27 -0800

    Clara's first guess number code

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,2 @@
-while True:
-    name = input("Enter Your Name: ")
-    print(name + " is having so much fun at the water park!She is eating ice cream and playing with her friends!She loves playing , she is having a great time!Her friends and her are making a video!") 
-
+name = input("Enter Your Name: ")
+print(name + " is having so much fun at the water park!She is eating ice cream and playing with her friends!She loves playing , she is having a great time!Her friends and her are making a video!") 
