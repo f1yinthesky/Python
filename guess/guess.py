1e2749fdfc8de46578a6fdc239803bbff7103d64
Fuzhuo Huang <huang.ford@gmail.com>
2024-12-23 20:19:58 -0800

    format

--- guess/guess.py
+++ guess/guess.py
@@ -1,10 +1,9 @@
-
 name = input("Hello! What is your name?\n")
 print(f"Well, {name}, I'm thinking of a number between 1 and 20.")
 
 target_number = 8
 number = int(input("Take a guess.\n"))
-count =1
+count = 1
 
 if number < target_number:
     print("Your guess is too low")
