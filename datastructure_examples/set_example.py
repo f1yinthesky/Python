158818bf5037bae9867a18400730998657e6249e
Fuzhuo Huang <huang.ford@gmail.com>
2025-01-26 18:48:51 -0800

    add set example

---
+++ datastructure_examples/set_example.py
@@ -0,0 +1,11 @@
+input_list = [1, 2, 1, 4, 5]
+print(f'input list: {input_list}')
+print(f'list length: {len(input_list)}')
+
+print(f'input_list[0] = {input_list[0]}')
+print(f'input_list[2] = {input_list[2]}')
+print(f'input_list[4] = {input_list[4]}')
+
+input_set = set(input_list)
+print(f'input set: {input_set}')
+print(f'set length: {len(input_set)}')
