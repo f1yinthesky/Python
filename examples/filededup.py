01407dd75b16b58ee1650bb5f3b8e0bfb31c4f0a
Fuzhuo Huang <huangford@gmail.com>
2025-03-11 21:12:37 -0700

    Modify filededup to avoid full hash if unnecessary

--- examples/filededup.py
+++ examples/filededup.py
@@ -1,27 +1,39 @@
-from typing import List, DefaultDict
+from typing import List, DefaultDict, Optional
 import os
 from collections import namedtuple, defaultdict
 import hashlib
 import filecmp 
-FileInfo = namedtuple("FileInfo", "file_size, small_hash, hash")
+FileInfo = namedtuple("FileInfo", "file_size, small_hash")
+
+def getFileHash(path:str, max_size:Optional[int] = None) -> str:
+    hasher = hashlib.sha256()
+    with open(path, "rb") as f:
+        if max_size is None:
+            hasher.update(f.read())
+        else:
+            hasher.update(f.read(max_size))
+    return hasher.hexdigest()
 
 def genFileInfo(entry: os.DirEntry) -> FileInfo:
     file_path = entry.path
     file_size = entry.stat().st_size
-    hasher = hashlib.sha256()
-    with open(file_path, "rb") as f:
-        hasher.update(f.read(1024))
-        small_hash = hasher.hexdigest()
-        hasher.update(f.read())
-        all_hash = hasher.hexdigest()
-    return (file_size, small_hash, all_hash)
+    return (file_size, getFileHash(file_path, 10))
+
+file_path_hash = {}
+def SlowCompareFile(left_file:str, right_file:str) -> bool:
+    if left_file not in file_path_hash:
+        file_path_hash[left_file] = getFileHash(left_file)
+    if right_file not in file_path_hash:
+        file_path_hash[right_file] = getFileHash(right_file)
+
+    if file_path_hash[left_file] != file_path_hash[right_file]:
+        return False
+    
+    return filecmp.cmp(left_file, right_file, shallow=False)
 
 def mergeDefaultDict(src: DefaultDict[FileInfo, List[str]], des: DefaultDict[FileInfo, List[str]]) -> None:
     for key, value in src.items():
-        if key not in des:
-            des[key] = value
-        else:
-            des[key].extend(value)
+        des[key].extend(value)
 
 def getAllFiles(one_dir:str) -> DefaultDict[FileInfo, List[str]]:
     all_files = defaultdict(list)
@@ -52,11 +64,11 @@ def duplicateFiles(one_dir: str) -> List[List[str]]:
                 right_file = file_list[j]
                 if right_file in used_file:
                     continue
-                if filecmp.cmp(left_file, right_file, shallow=False):
+                if SlowCompareFile(left_file, right_file):
                     left_duplicate.append(right_file)
                     used_file.add(right_file)
                 else:
-                    print("hash collision")
+                    print(f"hash collision between {left_file} and {right_file}")
             if len(left_duplicate) > 1:
                 answer.append(left_duplicate)
     return answer
@@ -64,7 +76,6 @@ def duplicateFiles(one_dir: str) -> List[List[str]]:
 def main() -> None:
     one_dir = r"C:\Users\huang\Downloads\tmp"
     print(duplicateFiles(one_dir))
-    print(filecmp.cmp('C:\\Users\\huang\\Downloads\\tmp\\1\\Clara G3 T2 report.pdf', 'C:\\Users\\huang\\Downloads\\tmp\\Clara G3 T2 report.pdf', shallow=False))
 
 if __name__ == '__main__':
     main()
